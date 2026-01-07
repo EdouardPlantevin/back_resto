89030ff352c62d42953cee2c6aaeed8e7e9fa1c1
EdouardPlantevin <plantevin.contact@gmail.com>
2026-01-07 14:43:12 +0100

    fix wk

--- src/main/java/com/edouard/back_resto/configuration/AppConfig.java
+++ src/main/java/com/edouard/back_resto/configuration/AppConfig.java
@@ -2,16 +2,25 @@ package com.edouard.back_resto.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.FileSystemResource;
+
+import java.io.File;
 
 @Configuration
 public class AppConfig {
 
     @Bean
+    @Profile("!test")
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
-        configurer.setLocation(new FileSystemResource(".env"));
+        File envFile = new File(".env");
+        if (envFile.exists()) {
+            configurer.setLocation(new FileSystemResource(envFile));
+        }
+        // Si le fichier .env n'existe pas, on ne configure pas le PropertySourcesPlaceholderConfigurer
+        // Les propriétés seront chargées depuis application.properties
         return configurer;
     }
 
